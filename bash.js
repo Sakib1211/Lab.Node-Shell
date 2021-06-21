e00b0fb07507fc667ab12ff11e5564da81a58ec2
Scott Scheine <scott.scheine@gmail.com>
2021-06-21 12:48:23 -0400

    updated bash.js and created pwd.js

---
+++ bash.js
@@ -0,0 +1,17 @@
+/*
+// Example 1 - create a prompt and output said text
+// Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim() // remove the newlne
+
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\nprompt > ');
+})
+
+*/
+
+const pwdFunc = require('./pwd');
+pwdFunc();
